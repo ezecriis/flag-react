d3c3abce29f177b222a3d6ae526ae09132e15119
Cris <cr.ledesma@hotmail.com>
2020-10-05 12:19:12 -0300

    Mobile version done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import CountryList from './components/CountryList';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { Region } from './components/Region';
+import reducer from './components/Reducer';
+import ActionList from './components/ActionsList';
+import Header from './components/Header';
 
 const initialState = {
   countryList: [],
@@ -12,45 +14,14 @@ const initialState = {
   filterByRegion: '',
 }
 
-function reducer(state, action) {
-  console.log(action)
-  switch (action.type) {
-    case 'SET_COUNTRY_LIST': {
-      console.log('actualzanndo')
-      return { ...state, countryList: action.payload }
-    }
-    case 'SET_COUNTRY_BY_NAME': {
-      const countryListByName = (state.countryList || [])
-        .filter(country => country.name.toLowerCase().includes(action.payload.toLowerCase()))
-      return { ...state, countryListByName }
-    }
-    case 'FILTER_BY_REGION': {
-      const { regionSelected } = action.payload;
-
-      if ('' === regionSelected) {
-        return { ...state, coutryFilteredByRegion: [], filterByRegion: '', };
-      }
-
-      const coutryFilteredByRegion = state.countryList.filter((country) => country.region === regionSelected);
-
-      return { ...state, coutryFilteredByRegion, filterByRegion: regionSelected }
-    }
-
-    default: {
-      return state;
-    }
-  }
-}
-
 const store = createStore(reducer, initialState)
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Region />
-        <CountryList />
-      </div>
+      <Header />
+      <ActionList />
+      <CountryList />
     </Provider>
   );
 }
